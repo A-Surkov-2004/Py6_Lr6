7371a48983d68a2011e4ab288d507a47eb32e27e
Andrey Surkov <andrey.surk.2904@gmail.com>
2025-05-02 02:11:45 +0300

    html gen

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 # distutils: extra_compile_args = -O3
 from setuptools import setup
 setup(
-    ext_modules = cythonize("cython_code.pyx")
+    ext_modules = cythonize("cython_code.pyx", annotate=True)
 )
